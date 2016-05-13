6644448ca29da14e544d5d2532607aedbf301ef5
Sergej Yakushenok <sergej_yakushenok@epam.com>
2016-05-13 10:40:23 +0300

    released memoize func

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,21 @@
-export { memoize };
+export {memoize};
 
 function memoize(func) {
-	return func; // TODO
+    var cache = {};
+    return chai.spy(function () {
+        var key = keyGen(arguments);
+        if (!(key in cache)) {
+            cache[key] = func.apply(this, arguments);
+        }
+        return cache[key];
+    });
+}
+function keyGen(value) {
+    var h = "";
+    if (value.length > 0) {
+        for (var i = 0; i < value.length; i++) {
+            h += 31 * i + value[i];
+        }
+    }
+    return h;
 }
