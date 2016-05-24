1b298fec3db8a8fb5ffee0eeb89adaff39ea2f25
Sergej Yakushenok <sergej_yakushenok@epam.com>
2016-05-24 13:01:26 +0300

    fix memoize function

--- src/memoize.js
+++ src/memoize.js
@@ -3,19 +3,10 @@ export {memoize};
 function memoize(func) {
     var cache = {};
     return function () {
-        var key = keyGen(arguments);
+        var key = JSON.stringify(arguments);
         if (!(key in cache)) {
             cache[key] = func.apply(this, arguments);
         }
         return cache[key];
     };
-}
-function keyGen(value) {
-    var h = "";
-    if (value.length > 0) {
-        for (var i = 0; i < value.length; i++) {
-            h += 31 * i + value[i];
-        }
-    }
-    return h;
 }
